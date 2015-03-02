ffc5cff89bf52df900349dd8bc3b0f4b49c09e5a
Ted Kassen <tedgkassen@gmail.com>
2015-03-02 13:54:21 -0800

    All but ec

--- app/src/main/java/com/cs646/ted/assignment3/ProfessorDetailFragment.java
+++ app/src/main/java/com/cs646/ted/assignment3/ProfessorDetailFragment.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -19,15 +21,23 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ProfessorDetailFragment extends Fragment {
 
     public static final String ARG_SEL_PROFESSOR_ID = "idofprofessorselected",
             ARG_PROFESSOR_GET_URL = "progfurl",
             ARG_RATING_POST_URL = "ratingpurl",
-            ARG_COMMENT_POST_URL = "commentpurl";
+            ARG_COMMENT_POST_URL = "commentpurl",
+            ARG_COMMENTS_GET_URL = "commentsgurl";
+
     public static final String FIRST_NAME = "firstName",
             LAST_NAME = "lastName",
             OFFICE = "office",
@@ -35,13 +45,16 @@ public class ProfessorDetailFragment extends Fragment {
             PHONE = "phone",
             RATING = "rating",
             AVERAGE = "average",
-            TOTAL = "totalRatings";
+            TOTAL = "totalRatings",
+            DATE = "date",
+            TEXT = "text";
 
     private int mProfessorId;
-    private String mProfessorURL, mRatingURL, mCommentURL;
+    private String mProfessorURL, mRatingURL, mCommentURL, mCommentsURL;
     private boolean mPopulated;
     private ProgressDialog mWaitDialog;
     private JSONObject mProfessorObject;
+    private JSONArray mCommentsArray;
     private TextView mLastName, mFirstName, mOffice, mPhone, mEmail, mAverageRating;
     private RatingBar mRating;
     private EditText mCommentEdit;
@@ -50,13 +63,15 @@ public class ProfessorDetailFragment extends Fragment {
     }
 
     public static ProfessorDetailFragment newInstance(int idOfProfSelected, String professorURL,
-                                                      String ratingURL, String commentURL) {
+                                                      String ratingURL, String commentURL,
+                                                      String commentsURL) {
         ProfessorDetailFragment fragment = new ProfessorDetailFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_SEL_PROFESSOR_ID, idOfProfSelected);
         args.putString(ARG_PROFESSOR_GET_URL, professorURL);
         args.putString(ARG_RATING_POST_URL, ratingURL);
         args.putString(ARG_COMMENT_POST_URL, commentURL);
+        args.putString(ARG_COMMENTS_GET_URL, commentsURL);
         fragment.setArguments(args);
         return fragment;
     }
@@ -69,6 +84,7 @@ public class ProfessorDetailFragment extends Fragment {
             mProfessorURL = getArguments().getString(ARG_PROFESSOR_GET_URL);
             mRatingURL = getArguments().getString(ARG_RATING_POST_URL);
             mCommentURL = getArguments().getString(ARG_COMMENT_POST_URL);
+            mCommentsURL = getArguments().getString(ARG_COMMENTS_GET_URL);
         }
     }
 
@@ -117,6 +133,25 @@ public class ProfessorDetailFragment extends Fragment {
         }
     }
 
+    public void setComments() throws JSONException {
+        ArrayList<String> commentsArray = new ArrayList<String>();
+
+        for (int i = 0; i < mCommentsArray.length(); i++) {
+            String newString = ((JSONObject) mCommentsArray.get(i)).get(DATE) + ": " +
+                    ((JSONObject) mCommentsArray.get(i)).get(TEXT);
+            commentsArray.add(newString);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_activated_1,
+                commentsArray);
+
+        ListView commentsList = (ListView) getView()
+                .findViewById(R.id.comment_list_view);
+
+        commentsList.setAdapter(adapter);
+    }
+
     private class FetchProfessorDetails extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
@@ -124,15 +159,25 @@ public class ProfessorDetailFragment extends Fragment {
                 HttpClient httpClient = new DefaultHttpClient();
 
                 String specificURL = mProfessorURL + Integer.toString(mProfessorId);
+                String commentsURL = mCommentsURL + Integer.toString(mProfessorId);
 
-                HttpGet httpGet = new HttpGet(specificURL);
-                HttpResponse response;
+                HttpGet httpProfessorDetailsGet = new HttpGet(specificURL);
+                HttpGet httpProfessorCommentsGet = new HttpGet(commentsURL);
+                HttpResponse detailsResponse, commentResponse;
 
                 try {
-                    response = httpClient.execute(httpGet);
+                    detailsResponse = httpClient.execute(httpProfessorDetailsGet);
 
                     mProfessorObject =
-                            new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8"));
+                            new JSONObject(EntityUtils
+                                    .toString(detailsResponse.getEntity(), "UTF-8"));
+
+                    commentResponse = httpClient.execute(httpProfessorCommentsGet);
+
+                    mCommentsArray =
+                            new JSONArray(EntityUtils
+                                    .toString(commentResponse.getEntity(), "UTF-8"));
+
                     mPopulated = true;
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -164,15 +209,18 @@ public class ProfessorDetailFragment extends Fragment {
 
             mAverageRating.setText("Average " + Double.toString(average) + " of " +
                     Integer.toString(total) + " ratings");
+
+            setComments();
         }
     }
 
     private class CommentAndSubmitRating extends AsyncTask<Void, Void, Void> {
         JSONObject mNewRating;
+        String mNewComment;
 
         @Override
         protected Void doInBackground(Void... params) {
-            String commentToSend = mCommentEdit.getText().toString();
+            mNewComment = mCommentEdit.getText().toString();
             Integer ratingToSend = mRating.getNumStars();
 
             HttpClient httpClient = new DefaultHttpClient();
@@ -185,7 +233,7 @@ public class ProfessorDetailFragment extends Fragment {
             HttpPost httpCommentPost = new HttpPost(postCommentURL);
 
             try {
-                httpCommentPost.setEntity(new StringEntity(commentToSend));
+                httpCommentPost.setEntity(new StringEntity(mNewComment));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -193,7 +241,7 @@ public class ProfessorDetailFragment extends Fragment {
             HttpResponse ratingResponse, commentResponse;
 
             try {
-                if (commentToSend != null) {
+                if (mNewComment != null) {
                     commentResponse = httpClient.execute(httpCommentPost);
                     EntityUtils.toString(commentResponse.getEntity(), "UTF-8");
                 }
@@ -223,6 +271,25 @@ public class ProfessorDetailFragment extends Fragment {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+            }
+
+            if (mNewComment != null) {
+
+                DateFormat dateFormat = new SimpleDateFormat("MM/dd/yy");
+                Date date = new Date();
+                System.out.println(dateFormat.format(date));
+
+                JSONObject newComment = new JSONObject();
+                try {
+                    newComment.put(DATE, dateFormat.format(date));
+                    newComment.put(TEXT, mNewComment);
+                    mCommentsArray.put(newComment);
+                    setComments();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                mCommentEdit.setText("");
             }
         }
     }
